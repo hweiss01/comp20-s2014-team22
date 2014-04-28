ebe0928ab7c4dc7ef6fd78fcd2105d68527383dd
Justin Shiiba <jshiiba@gmail.com>
2014-04-28 13:38:15 -0400

    testing Jade

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { 'title': 'Express', 'purchases': [
+  							{'website': 'Amazon', 'date': '4/1/14', 'price': '50'},
+  							{'website': 'Amazon', 'date': '5/20/14', 'price': '25'}
+  						]});
 };

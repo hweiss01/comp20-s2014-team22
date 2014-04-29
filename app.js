9846049856450130fabf3a9effbcedb5df7879dd
Justin Shiiba <jshiiba@gmail.com>
2014-04-29 12:24:35 -0400

    Added stylesheet to retrieve page

--- app.js
+++ app.js
@@ -5,17 +5,17 @@
 //Cross-Origin Resource Sharing (CORS)
 //source: http://bannockburn.io/2013/09/cross-origin-resource-sharing-cors-with-a-node-js-express-js-and-sencha-touch-app/
 var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
  
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
+		// intercept OPTIONS method
+		if ('OPTIONS' == req.method) {
+			res.send(200);
+		}
+		else {
+			next();
+		}
 };
 
 var express = require('express');
@@ -35,12 +35,12 @@ var mongo = require('mongodb');
 
 //MongoDB Setup
 var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/myDatabase';
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/myDatabase';
 
 //connect to Mongo globally
 var db = mongo.Db.connect(mongoUri, function (err, database) {
-  db = database;
+	db = database;
 });
 
 
@@ -59,7 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
@@ -71,59 +71,61 @@ COLLECTION = WILL
 DOCUMENT: JSON object; contains arrays of tuples
 
 WILL
-  {date:1/1/11, items:[[d,w,p], [d,w,p]]}
-  {[d,w,p]}
-  {[d,w,p], [d,w,p], [d,w,p], [d,w,p]}
+	{date:1/1/11, items:[[d,w,p], [d,w,p]]}
+	{[d,w,p]}
+	{[d,w,p], [d,w,p], [d,w,p], [d,w,p]}
 */
 
 
 // GET /retrieve
 app.get('/retrieve', function(req, res) {
-  // var user = req.query.username; //from /retrieve?username=SOMEDATA
-  // console.log(user);
+	// var user = req.query.username; //from /retrieve?username=SOMEDATA
+	// console.log(user);
 
-  // db.collection(user, function(er, collection) {
+	// db.collection(user, function(er, collection) {
 
-  //   //sort documents in <user> collection
-  //   collection.find().toArray(function (err, docs) {
-  //     var purchaseData = sortResults(docs, "date", false); //possible fields: data, website, price
+	//   //sort documents in <user> collection
+	//   collection.find().toArray(function (err, docs) {
+	//     var purchaseData = sortResults(docs, "date", false); //possible fields: data, website, price
 
-  //     var output = []; //holds JSON objects
+	//     var output = []; //holds JSON objects
 
-  //     //find data for correct user
-  //     for (doc in purchaseData) {
-  //       for (item in purchaseData[doc]) {
-  //         output.push(purchaseData[doc][item]);
-  //       }
-  //     }
+	//     //find data for correct user
+	//     for (doc in purchaseData) {
+	//       for (item in purchaseData[doc]) {
+	//         output.push(purchaseData[doc][item]);
+	//       }
+	//     }
 
-  //     res.send(output); //[ [d,w,p], [d,w,p], ... ]
-  //   });
-  // });
-    res.render('retrieve', { 'purchases': [
-            {'website': 'Amazon', 'date': '4/1/14', 'price': '50'},
-            {'website': 'Amazon', 'date': '5/20/14', 'price': '25'}
-          ]});
+	//     res.send(output); //[ [d,w,p], [d,w,p], ... ]
+	//   });
+	// });
+	var total = '$75'
+		res.render('retrieve', { 'purchases': [
+						{'website': 'Amazon', 'date': '4/1/14', 'price': '50'},
+						{'website': 'Amazon', 'date': '5/20/14', 'price': '25'}
+					], 'total': total
+					});
 });
 
 
 // POST /submit.json
 app.post('/submit.json', function(req, res) {
-  var JSONstring = req.body["data"];
-  var buffer = JSON.parse(JSONstring);
+	var JSONstring = req.body["data"];
+	var buffer = JSON.parse(JSONstring);
 
-  //CLEAN THE BUFFER (SECURITY) !!!
-  
-  db.collection(buffer["user"], function(er, collection) {
-      for (key in buffer["purchases"]) {
-        if (collection.find({"confirmation":buffer["purchases"][key]["confirmation"]}) == null) { //confirmation is not in database
-          collection.insert({"date":buffer["purchases"][key]["date"], "items":buffer["purchases"][key]["items"]});
-          console.log("IM INSERTING A DOCUMENT");
-        }
-      }
-  });
+	//CLEAN THE BUFFER (SECURITY) !!!
+	
+	db.collection(buffer["user"], function(er, collection) {
+			for (key in buffer["purchases"]) {
+				if (collection.find({"confirmation":buffer["purchases"][key]["confirmation"]}) == null) { //confirmation is not in database
+					collection.insert({"date":buffer["purchases"][key]["date"], "items":buffer["purchases"][key]["items"]});
+					console.log("IM INSERTING A DOCUMENT");
+				}
+			}
+	});
 
-  res.send("Posted scores to database.");
+	res.send("Posted scores to database.");
 });
 
 
@@ -131,14 +133,14 @@ app.post('/submit.json', function(req, res) {
 //Sorts JSON Array <sortThis> by <prop>, in asc/desc order
 //modified from Sean the Bean: http://stackoverflow.com/questions/881510/jquery-sorting-json-by-properties
 function sortResults(sortThis, prop, asc) {
-    sortThis = sortThis.sort(function(a, b) {
-        if (asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
-        else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
-    });
-    return sortThis;
+		sortThis = sortThis.sort(function(a, b) {
+				if (asc) return (a[prop] > b[prop]) ? 1 : ((a[prop] < b[prop]) ? -1 : 0);
+				else return (b[prop] > a[prop]) ? 1 : ((b[prop] < a[prop]) ? -1 : 0);
+		});
+		return sortThis;
 }
 
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+	console.log('Express server listening on port ' + app.get('port'));
 });

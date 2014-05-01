8a5d7eb1b1bb3f3e932592212a9288df3da28aca
Hayley Weiss <Hayley.Weiss@tufts.edu>
2014-04-30 22:18:56 -0400

    graphs

--- public/javascripts/retrieve.js
+++ public/javascripts/retrieve.js
@@ -11,11 +11,8 @@ function hello(){
 			{console.log('Context not supported');}
 		else{
 			var context = ctx.getContext('2d');
-			console.log(ctx);
-			console.log(context);
 
-			//Purchase = ['{"website":"Amazon", "date":"1/12/14", "price":50}', '{"website":"Ebay", "date":"1/13/14", "price":70}'];
-
+			
 			labels = [];
 			data1 = [];
 
@@ -26,20 +23,12 @@ function hello(){
 			
 			for(var i = 1; i < height; i++){
 
-					console.log(table.rows[i].cells[1].innerHTML);
-					console.log(table.rows[i].cells[2].innerHTML);
 					labels.push(table.rows[i].cells[1].innerHTML);
-					data1.push(table.rows[i].cells[2].innerHTML);
+					data1.push(parseFloat(table.rows[i].cells[2].innerHTML));
 
 				}
-				
-			/*Purchase.map (function (str) {
-				datum = JSON.parse(str);
-				labels.push(datum.date);
-				data1.push(datum.price);
-			});
-*/
 
+			
 
 			var myNewChart = new Chart(context);
 			var data = {
@@ -48,13 +37,13 @@ function hello(){
 					{
 						fillColor : "rgba(220,220,220,0.5)",
 						strokeColor : "rgba(220,220,220,1)",
-						//pointColor : "rgba(220,220,220,1)",
-						//pointStrokeColor : "#fff",
+						pointColor : "rgba(220,220,220,1)",
+						pointStrokeColor : "#fff",
 						data : data1
 					}
 				]
 			}
-		new Chart(context).Bar(data);
+		new Chart(context).Line(data);
 
 		}
 	}
